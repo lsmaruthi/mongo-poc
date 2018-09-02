c441307e902f05f1cd1c0822956f829e02abd522
Lattala <469339@cognizant.com>
2018-09-03 01:24:47 +0530

    added test suite for todo api

--- server/server.js
+++ server/server.js
@@ -23,3 +23,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
 	console.log('app running')
 })
+
+module.exports = {app};
---
+++ server/test/server.test.js
@@ -0,0 +1,48 @@
+var expect = require('expect');
+var request = require('supertest');
+var {app} = require('./../server');
+var {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+	Todo.remove({}).then(() => done());
+})
+
+describe('POST /todos', () => {
+	it('should create a new todo', (done) => {
+		var text = 'todo test api';
+		request(app)
+		.post('/todos')
+		.send({text})
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.text).toBe(text);
+		})
+		.end((err, res) => {
+			if(err) 
+				return done(err)
+
+			Todo.find().then((docs) => {
+				expect(docs.length).toBe(1);
+				expect(docs[0].text).toBe(text);
+				done();
+			}).catch((e) => done(e))
+		})
+	})
+
+	it('should not create a new todo', (done) => {
+		var text = 'todo test api';
+		request(app)
+		.post('/todos')
+		.send({})
+		.expect(400)
+		.end((err, res) => {
+			if(err) 
+				return done(err)
+
+			Todo.find().then((docs) => {
+				expect(docs.length).toBe(0);
+				done();
+			}).catch((e) => done(e))
+		})
+	})
+})
