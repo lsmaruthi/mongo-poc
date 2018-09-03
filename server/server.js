3990d044ef81ecc6221fbdeaefffac937b079293
Lattala <469339@cognizant.com>
2018-09-04 00:08:25 +0530

    add patch todo

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var mongoose = require('./db/mongoose-db');
+const _ = require('lodash');
 var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -54,6 +55,24 @@ app.delete('/todos/:id', (req, res) => {
 		res.send({todo});
 	}, (err) => {
 		res.status(400).send(err);
+	})
+})
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+	if(!ObjectID.isValid(id))
+		return res.status(404).send({});
+	if(_.isBoolean(body.completed) && body.completed)
+		body.completedAt = new Date().getTime();
+	else 
+		body.completed = false;
+		body.completedAt = null;
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo)
+			return res.status(404).send({});
+		res.send({todo})
 	})
 })
 
