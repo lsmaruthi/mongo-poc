6304a1126cdd9cb9f0700dc5dc46dba8b5552d72
Lattala <469339@cognizant.com>
2018-09-06 13:22:47 +0530

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,18 @@
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+var password = "abc123";
+/*bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hashVal) => {
+		console.log(hashVal)
+	})
+}); */
+
+bcrypt.compare(password, '$2a$10$nEZZAkIgpgclu.JKrnx2QObHoTu7bgGGUJyqxnfdWHN8NmJED/fDW', (err, res) => {
+	console.log(res)
+})
+/*
 var data = {
 	id: 10
 }
@@ -9,4 +21,4 @@ var token = jwt.sign(data, '123abc');
 console.log(token)
 
 var decoded = jwt.verify(token, '123abc');
-console.log(decoded)
+console.log(decoded)*/
